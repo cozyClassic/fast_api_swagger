017eab0ddbb3e4c63f3089fff0fa417857cf5dc1
KIM <km0192@naver.com>
2022-07-28 14:15:25 +0900

    feat: add column deleted_at

--- sql/0.0.4_create_table_body_monitor.sql
+++ sql/0.0.4_create_table_body_monitor.sql
@@ -9,6 +9,7 @@ CREATE TABLE body_monitor (
     max_bp INT UNSIGNED COMMENT '최고혈압',
     min_bp INT UNSIGNED COMMENT '최저혈압',
     pulse INT UNSIGNED COMMENT '맥박',
+    deleted_at TIMESTAMP,
     PRIMARY KEY (`id`),
     KEY `body_monitor_fk_1` (`user_id`),
     CONSTRAINT `body_monitor_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
